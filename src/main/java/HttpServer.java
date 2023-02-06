9e935845e4c226a6fae7bf3f7334c0609bfaeb05
Sridevi <sridevi.elangovan@gcitsolutions.com>
2023-02-06 19:51:42 +0530

    Updated Web Server Program - Single Thread * Added README.md file in the program * Added logger (java.util.logging) * Added logging.properties file to format log in the program * added new class colorLogger for log console output * added new class httpServer, in which http response method are defined * called httpServer in main class Server

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpServer {
+    static void handleClient(Socket client) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
+        }
+
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String method = requestLine[0];
+        String path = requestLine[1];
+        String version = requestLine[2];
+        String host = requestsLines[1].split(" ")[1];
+
+        List<String> headers = new ArrayList<>();
+        for (int h = 2; h < requestsLines.length; h++) {
+            String header = requestsLines[h];
+            headers.add(header);
+        }
+
+        String accessLog = String.format("Client %s, method %s, path %s, version %s, host %s, headers %s",
+                client.toString(), method, path, version, host, headers.toString());
+        System.out.println(accessLog);
+
+
+        Path filePath = getFilePath(path);
+        if (Files.exists(filePath)) {
+            // file exist
+            String contentType = guessContentType(filePath);
+            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
+        } else {
+            // 404
+            badResponse(client, "404 Not Found");
+
+        }
+
+    }
+
+    private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write(("HTTP/1.1 " + status + "\r\n").getBytes());
+        clientOutput.write(("ContentType: " + contentType + "\r\n").getBytes());
+        clientOutput.write("\r\n".getBytes());
+        clientOutput.write(content);
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        clientOutput.close();
+        client.close();
+    }
+
+    private static void badResponse(Socket client, String status) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write(("HTTP/1.1 " + status + "\r\n").getBytes());
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        clientOutput.close();
+        client.close();
+    }
+
+    private static Path getFilePath(String path) {
+        if ("/".equals(path)) {
+            path = "/index.html";
+        }
+
+        return Paths.get("D:/Projects/HTML/sample", path);
+    }
+
+    private static String guessContentType(Path filePath) throws IOException {
+        return Files.probeContentType(filePath);
+    }
+}
+
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.logging.LogManager;
@@ -16,107 +14,30 @@ public class Server {
         try {
             LogManager.getLogManager().readConfiguration(stream);
 
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(8085);
-
-        colorLogger.logInfo("server started");
-        Instant start = Instant.now();
-        colorLogger.logInfo("Starting Time : " +start);
-        while (true) {
-            listenToClientConnections(serverSocket);
-            Instant end = Instant.now();
-            Duration timeElapsed = Duration.between(start, end);
-            colorLogger.logInfo("Time Duration : " +timeElapsed);
-        }
-
-    }
-
-    public static void listenToClientConnections(ServerSocket serverSocket) {
-        try {
-            Socket clientSocket = serverSocket.accept();
-            colorLogger.logInfo("client send request");
-
-
-//            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            File file = new File("D:\\Projects\\csweb\\index.html");
-
-            FileInputStream inputStream = new FileInputStream(file);
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String requestedResource = "";
-                String incomingLineFromClient;
-                while ((incomingLineFromClient = in.readLine()) != null) {
-                    System.out.println(incomingLineFromClient);
-
-                    if (incomingLineFromClient.contains("HTTP/1.1")) {
-                        requestedResource = incomingLineFromClient;
-                    }
-
-                    if (incomingLineFromClient.equals(""))
-                        break;
+        try (ServerSocket serverSocket = new ServerSocket(8085)) {
+            while (true) {
+                colorLogger.logInfo("Server Started ");
+                try (Socket client = serverSocket.accept()) {
+                    Instant start = Instant.now();
+                    colorLogger.logInfo("Starting Time : " + start);
+                    HttpServer.handleClient(client);
+                    Instant end = Instant.now();
+                    colorLogger.logInfo("End Time : " + end);
+                    long elapsedTime = Duration.between(start, end).toMillis();
+                    colorLogger.logInfo("Duration of Request and Response : " + elapsedTime + " milliseconds");
+                }catch (IOException e){
+                    e.printStackTrace();
                 }
-
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-                if (file.exists()) {
-                   String response = "You have requested this resource: " + requestedResource;
-
-                    out.print("HTTP/1.1 200 OK\n");
-                    out.print("Content-Length: " + response.length() + "\n");
-                    out.print("Content-Type: text/html; charset=utf-8\n");
-                    out.print("Date: Tue, 25 Oct 2016 08:17:59 GMT\n");
-                    out.print("\n");
-                    out.print(response);
-                    out.print(inputStream.readAllBytes());
-                    out.flush();
-
-                } else {
-
-                    out.print("HTTP/1.1 404 NOT FOUND\n");
-                }
-            } catch (IOException e) {
-                colorLogger.logError("Error connecting to client" + e);
-                throw new RuntimeException(e);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
-
 }
-
-//            OutputStream os = clientSocket.getOutputStream();
-//            File file = new File("D:\\Projects\\csweb\\index.html");
-//
-//
-//            try (FileInputStream fin = new FileInputStream(file);
-//                     BufferedReader br = new BufferedReader(new InputStreamReader(fin));) {
-//                    if (br.ready()) {
-//                        String line1 = br.readLine();
-//                        System.out.println(line1);
-//                    }
-////            OutputStream os = clientSocket.getOutputStream();
-//                    os.write("HTTP/1.1 200 OK\r\n".getBytes());
-//                    os.write("\r\n".getBytes());
-//                    os.write(fin.readAllBytes());
-//                    os.flush();
-//                }
-//
-//            } catch (FileNotFoundException ex) {
-//                System.out.println("Info.xml is not found");
-//            } catch (IOException ex) {
-//                System.out.println("Can't read the file");
-//            }
-
-
-//        Read more: https://javarevisited.blogspot.com/2014/04/10-jdk-7-features-to-revisit-before-you.html#ixzz7oBm7RCKO
 
 
